94c8537a6461adceff6563bb106362adf6894426
Jems Dorviene <dorviene@gmail.com>
2025-03-22 12:36:01 -0700

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,11 @@
-/* GET Homepage */
-const index = (req, res) => {
-    res.render('travel', {title: "travlr Getaways"});
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+/* GET travel view */
+const travel = (req, res) => {
+    res.render('travel', {title: 'Travlr Getaways',trips});
 };
 
 module.exports = {
-    index
+    travel
 };
