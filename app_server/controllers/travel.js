da928cdc8ac44a7a8596728673d701689b5f28ee
Jems Dorviene <dorviene@gmail.com>
2025-04-07 10:52:59 -0700

    Module 5 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,39 @@
+const tripsEndpoint = 'http://localhost:3000/api/trips';  
+const options = {  
+    method: 'GET',
+    headers: {  
+        'Accept': 'application/json'
+    }  
+}  
+
+// var fs = require('fs');  
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
 var fs = require('fs');
 var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
 
 /* GET travel view */
-const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways',trips});
-};
+const travel = async function(req, res, next) {
+    // console.log('Travel CONTROLLER BEGIN');
+    await fetch(tripsEndpoint, options)
+        .then(res => res.json())
+        .then(json => { 
+            // console.log(json)
+            let message = null;
+            if(!(json instanceof Array)){
+                message = 'API lookup error';
+                json = [];
+            } else {
+                if(!json.lenght){
+                    message = 'No trips exist in our database!';
+                }
+            }
 
-module.exports = {
+            res.render('travel', {title: 'Travel Getaways', trips: json, message});
+        })
+        .catch(err => res.status(500).send(e.message));
+    // console.log('TRAVEL CONTROLLER AFTER RENDER');
+};
+module.export = {
     travel
 };
